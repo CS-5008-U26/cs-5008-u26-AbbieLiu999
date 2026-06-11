951e2ca5fcbfbdf7defd90c33840c88424d9587c
Dan Liu <abbieliu666@gmail.com>
2026-06-10 20:11:14 -0700

    Lab 4 added

---
+++ Day 4/Lab 4/first.h
@@ -0,0 +1,7 @@
+/* Header file for first.c */
+#ifndef FIRST_H
+#define FIRST_H
+
+void first(char* s);
+
+#endif
---
+++ Day 4/Lab 4/second.h
@@ -0,0 +1,7 @@
+/* Header file for second.c */
+#ifndef SECOND_H
+#define SECOND_H
+
+void second(char* s);
+
+#endif
